effed7ed029764c7f9a36629c006ad78901d395b
DagmawiKK <dagmawikassa1@gmail.com>
2024-07-17 19:20:32 +0300

    Create the game logic

--- javascript.js
+++ javascript.js
@@ -2,10 +2,43 @@ function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"];
     return choice[Math.ceil(Math.random() * (3)) -1];
 }
-console.log(getComputerChoice())
+
+
 
 function getHumanChoice() {
     let HumanChoice = prompt("Input your choice");
-    return HumanChoice;
+    return HumanChoice.toLowerCase();
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice != computerChoice) {
+        if (humanChoice == "rock" && computerChoice == "scissors") {
+            // humanScore++;
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+            // computerScore++;
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            // humanScore++;
+        }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            // computerScore++;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+            // computerScore++;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            // humanScore++;
+            console.log("h win");
+        }
+    }
+}
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+
+playRound(computerChoice, humanChoice);
