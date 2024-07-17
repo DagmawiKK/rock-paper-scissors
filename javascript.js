6c2e073797d118826b93676c83c0026fdb67c6ed
DagmawiKK <dagmawikassa1@gmail.com>
2024-07-17 19:38:35 +0300

    Implement game play

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"];
+    console.log(choice[Math.ceil(Math.random() * (3)) -1])
+
     return choice[Math.ceil(Math.random() * (3)) -1];
 }
 
-
-
 function getHumanChoice() {
     let HumanChoice = prompt("Input your choice");
-    return HumanChoice.toLowerCase();
+    console.log(HumanChoice)
+    return HumanChoice.toLowerCase().trim();
 }
 
 let humanScore = 0;
@@ -17,28 +18,44 @@ function playRound(humanChoice, computerChoice) {
 
     if (humanChoice != computerChoice) {
         if (humanChoice == "rock" && computerChoice == "scissors") {
-            // humanScore++;
+            humanScore++;
         }
         else if (humanChoice == "paper" && computerChoice == "scissors") {
-            // computerScore++;
+            computerScore++;
         }
         else if (humanChoice == "paper" && computerChoice == "rock") {
-            // humanScore++;
+            humanScore++;
         }
         else if (humanChoice == "rock" && computerChoice == "paper") {
-            // computerScore++;
+            computerScore++;
         }
         else if (humanChoice == "scissors" && computerChoice == "rock") {
-            // computerScore++;
+            computerScore++;
         }
         else if (humanChoice == "scissors" && computerChoice == "paper") {
-            // humanScore++;
-            console.log("h win");
+            humanScore++;
         }
     }
 }
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
 
+function playGame() {
+    let i = 5;
 
-playRound(computerChoice, humanChoice);
+    while (i > 0) {
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        playRound(computerChoice, humanChoice);
+        i--;
+    } 
+
+    if (humanScore > computerScore) {
+        console.log("The human won with value: " + humanScore);
+        return; 
+    }
+    else {
+        console.log("The computer won with value: " + computerScore);
+        return;
+    }
+}
+
+playGame()
