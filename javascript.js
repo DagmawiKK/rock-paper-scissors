98fe0988c942deb53e8da219c08d3e3640781195
DagmawiKK <dagmawikassa1@gmail.com>
2024-07-30 13:27:18 +0300

    Finished UI

--- javascript.js
+++ javascript.js
@@ -1,99 +1,87 @@
+let container = document.querySelector(".container");
+
+let rock = document.createElement("button");
+let paper = document.createElement("button");
+let scissors = document.createElement("button");
+
+rock.textContent = "rock";
+paper.textContent = "paper";
+scissors.textContent = "scissors";
+
+rock.id = "rock";
+paper.id = "paper";
+scissors.id = "scissors";
+
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissors);
+
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"];
     return choice[Math.ceil(Math.random() * (3)) -1];
-}
-
-function getHumanChoice() {
-    let HumanChoice = prompt("Input your choice");
-    console.log(HumanChoice)
-    return HumanChoice.toLowerCase().trim();
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-
-    if (humanChoice != computerChoice) {
-        if (humanChoice == "rock" && computerChoice == "scissors") {
-            humanScore++;
-        }
-        else if (humanChoice == "paper" && computerChoice == "scissors") {
-            computerScore++;
-        }
-        else if (humanChoice == "paper" && computerChoice == "rock") {
-            humanScore++;
-        }
-        else if (humanChoice == "rock" && computerChoice == "paper") {
-            computerScore++;
-        }
-        else if (humanChoice == "scissors" && computerChoice == "rock") {
-            computerScore++;
-        }
-        else if (humanChoice == "scissors" && computerChoice == "paper") {
-            humanScore++;
-        }
+    if (humanChoice === computerChoice) {
+        return "Current round info: It's a tie";
     }
+    if ((humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper"))
+    {
+        humanScore++;
+        return "Current round info: You won the round";
+    }
+    else
+    {
+        computerScore++;
+        return "Current round info: Computer won the round";
+    }
+
 }
 
 function playGame() {
 
+    let result = document.querySelector(".result");
+    let total = document.querySelector(".total");
 
+    container.addEventListener('click', (event) => {
+        let target = event.target;
         let computerChoice = getComputerChoice();
-
-        let container = document.querySelector(".container");
-
-        let rock = document.createElement("button");
-        let paper = document.createElement("button");
-        let scissors = document.createElement("button");
-
-        rock.textContent = "rock";
-        paper.textContent = "paper";
-        scissors.textContent = "scissors";
-
-        rock.id = "rock";
-        paper.id = "paper";
-        scissors.id = "scissors";
-
-        container.appendChild(rock);
-        container.appendChild(paper);
-        container.appendChild(scissors);
-
-        container.addEventListener('click', (event) => {
-            let target = event.target;
-
-            switch (target.id) {
-                case "rock":
-                    playRound(computerChoice, "rock");
-                    break
-                case "paper":
-                    playRound(computerChoice, "paper");
-                    break
-                case "scissors":
-                    playRound(computerChoice, "scissors");
-                    break
-            }
-        })
-
-
-
+        switch (target.id) {
+            case "rock":
+                 humanChoice= "rock";
+                break
+            case "paper":
+                humanChoice = "paper";
+                break
+            case "scissors":
+                humanChoice = "scissors";
+                break
         
-        let humanChoice = getHumanChoice();
-        alert("computer played: " + computerChoice);
-        alert("Current score is Computer: " + computerScore + " Human: " + humanScore);   
+        }
 
-        playRound(computerChoice, humanChoice);
-        i--;
-    } 
+        result.textContent = playRound(humanChoice, computerChoice);
+        
+        if (humanScore < 5 && computerScore < 5) {
+            total.textContent = `Total Score of the game is you with ${humanScore} and computer with ${computerScore}`;
+        }
+        else {
+            if (humanScore == 5) {
+            total.textContent = ` You won with a total result ${humanScore}`;
+            }
+            else if (computerScore == 5) {
+                total.textContent = `Computer won with a total result ${computerScore}`;
+            }
+            computerScore = 0;
+            humanScore = 0;
+        }
 
-    if (humanScore > computerScore) {
-        alert("The human won with value: " + humanScore);
-        return; 
-    }
-    else {
-        alert("The computer won with value: " + computerScore);
-        return;
-    }
+    })
+
 }
 
 playGame()
